6b74d62beafd96d9b5abd03a8ba8ff0ab880a54a
Herman Gouw <hermangouw@gmail.com>
2014-09-08 08:07:57 +1000

    Added comments

--- Challenge6.cs
+++ Challenge6.cs
@@ -5,6 +5,7 @@ namespace Challenges
     public static class Challenge6
     {
         /// <summary>
+        /// Challenge 6
         /// A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
         /// Count the minimal number of jumps that the small frog must perform to reach its target.
         /// X, Y and D are integers within the range [1..1,000,000,000];
